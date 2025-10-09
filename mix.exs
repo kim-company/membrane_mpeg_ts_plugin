bbe0efe22fe3d521b0b560101f00e016f4ea878b
Daniel Morandini <danielmorandini@me.com>
2025-10-09 11:02:07 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Membrane.MPEG.TS.MixProject do
   def project do
     [
       app: :membrane_mpeg_ts_plugin,
-      version: "2.3.1",
+      version: "2.3.2",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
